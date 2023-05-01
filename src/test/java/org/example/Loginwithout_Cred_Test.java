218601e91198473f4ff31622a957d5e3c29edb90
Mohi <mohiuddin3551@gmail.com>
2023-05-01 20:41:35 +0530

    done

---
+++ src/test/java/org/example/Loginwithout_Cred_Test.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+@Test
+public class Loginwithout_Cred_Test
+{
+    WebDriver driver;
+    public void Test() throws InterruptedException {
+
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        Loginwith_withoutCred obj = new Loginwith_withoutCred(driver);
+        driver.get("https://www.ebay.com/signin/");
+        driver.manage().window().maximize();
+        Thread.sleep(10000);
+        obj.clik();
+    }
+}
---
+++ src/test/java/org/example/Username_Change_Test.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+@Test
+public class Username_Change_Test {
+
+    WebDriver driver;
+    public void test() throws InterruptedException
+    {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        Username_Change obj = new Username_Change(driver);
+        driver.get("https://www.ebay.com/signin/");
+        driver.manage().window().maximize();
+        Thread.sleep(10000);
+        obj.login("sharmaboymanjeet123@gmail.com","qpalzm@1M");
+        obj.usernamechange();
+    }
+}
